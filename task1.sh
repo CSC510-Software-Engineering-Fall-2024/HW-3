5e69481548f38bf8224c3518479e6cc5ea34af6d
archig97 <62295366+archig97@users.noreply.github.com>
2024-10-10 19:40:11 +0000

    changed to one-line code with pipes and logical operators

--- task1.sh
+++ task1.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Find the PID of the infinite.sh script
-pid=$(ps aux | grep '[i]nfinite.sh' | awk '{print $2}')
 
-# Check if PID is found
-if [ -z "$pid" ]; then
-    echo "infinite.sh is not running."
-else
-    # Kill the process
-    kill -9 $pid
-    echo "infinite.sh (PID $pid) has been terminated."
-fi
+pid=$(ps aux | grep '[i]nfinite.sh' | awk '{print $2}') && [ -z "$pid" ] && echo "infinite.sh is not running." || (kill -9 $pid && echo "infinite.sh (PID $pid) has been terminated.")
