7eab14a16037ecefbb791f8a08099cc2b1b9e8df
archig97 <62295366+archig97@users.noreply.github.com>
2024-10-10 23:47:49 +0000

    changes to Task 2

--- task2.sh
+++ task2.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-# Directory to search for files
-DIR="./dataset1"
-
-results=()
-
-for file in "$DIR"/*; do
-  # Check if it's a file
-  if [[ -f "$file" ]]; then
-    if grep -q "sample" "$file"; then
-      count=$(grep -o "CSC510" "$file" | wc -l)
-      size=$(stat -c%s "$file")
-      if [[ "$count" -ge 3 ]]; then
-        results+=("$count $size $file")
-      fi
-    fi
-  fi
-done
-
-sorted_results=$(printf "%s\n" "${results[@]}" | gawk '{print $0}' | sort -k1,1nr -k2,2nr)
-echo "$sorted_results" | awk '{sub(/file_/, "filtered_", $3); print $3}'
+grep -rl "sample" . | xargs -I{} sh -c 'count=$(grep -o "CSC510" {} | wc -l); size=$(stat -f%z "{}"); if [ "$count" -ge 3 ]; then echo "$count $size {}"; fi' | sort -k1,1nr -k2,2nr | gawk '{sub(/file_/, "filtered_", $3); print $3}'
