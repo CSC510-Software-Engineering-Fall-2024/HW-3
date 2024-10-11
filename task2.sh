83a397596a8e0ff20df9b72e2a858d1e3278316f
archig97 <62295366+archig97@users.noreply.github.com>
2024-10-11 03:49:15 +0000

    task2 changes

--- task2.sh
+++ task2.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-grep -rl "sample" . | xargs -I{} sh -c 'count=$(grep -o "CSC510" {} | wc -l); size=$(stat -f%z "{}"); if [ "$count" -ge 3 ]; then echo "$count $size {}"; fi' | sort -k1,1nr -k2,2nr | gawk '{sub(/file_/, "filtered_", $3); print $3}'
-
+grep -l "sample" dataset1/* | xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then size=$(stat -c%s "{}"); echo "$count $size {}"; fi' | sort -k1,1nr -k2,2nr | gawk '{sub("file_", "filtered_", $3); print $0}'
